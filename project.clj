4a80ee81031676981f3e794b031281ee31ec448a
Daniel Westheide <daniel@doo.net>
2012-08-29 11:26:54 +0200

    Release as 0.2.1-PATCHED.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.doo/ring-anti-forgery "0.2.1-SNAPSHOT"
+(defproject org.clojars.doo/ring-anti-forgery "0.2.1-PATCHED"
   :description "Ring middleware to prevent CSRF attacks"
   :url "https://github.com/weavejester/ring-anti-forgery"
   :license {:name "The MIT License"
